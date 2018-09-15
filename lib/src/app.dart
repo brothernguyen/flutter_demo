cd6a9353816d5bc629e01ceef2306b41195cff74
Anh Nguyen <anh.tuan.nguyen@terralogic.com>
2018-09-15 19:28:56 +0700

    First commit

---
+++ lib/src/app.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' show get;
+import 'models/image_model.dart';
+
+class App extends StatefulWidget {
+  createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  int counter = 0;
+  void fetchImage() async {
+    counter ++;
+    var response = await get('https://jsonplaceholder.typicode.com/photos/$counter');
+    
+  }
+
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Text('$counter'),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            fetchImage();
+          },
+        ),
+        appBar: AppBar(
+          title: Text('Let see some images'),
+        ),
+      ),
+    );
+  }
+}
